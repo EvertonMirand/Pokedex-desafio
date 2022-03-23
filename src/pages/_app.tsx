861bbb978268e5f5e2e8565c599abec79a2c1424
EvertonMirand <everton.mirandav@gmail.com>
2022-03-22 21:36:16 -0300

    Configura styled components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
 import type { AppProps } from 'next/app';
+import { Fragment } from 'react';
+import Global from '../styles/global';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Fragment>
+      <Component {...pageProps} />
+      <Global />
+    </Fragment>
+  );
 }
 
 export default MyApp;
